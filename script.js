7d9f0064499173e7697606ac08a16f97c9dc0797
onna3312 <ilyazor2@gmail.com>
2024-02-03 01:23:40 +0300

    Add grid resize functionality

--- script.js
+++ script.js
@@ -1,26 +1,60 @@
 const gridContainer = document.querySelector(".grid-container");
+const gridButton = document.querySelector(".grid-button");
+let defaultBlockSize = 32;
+let newSize = null;
+let defaultGridSize = 16;
+let newGridSize = null;
+
 
 function fillGridContainer() {
-  for (let i = 1; i <= 256; i++) {
+  if (newSize !== null) {
+    defaultBlockSize = newSize;
+  }
+
+  if (newGridSize !== null) {
+    defaultGridSize = newGridSize;
+  }
+
+  for (let i = 1; i <= defaultGridSize**2; i++) {
     let gridBlock = document.createElement("div");
-    gridBlock.style.height = "32px";
-    gridBlock.style.width = "32px";
+    gridBlock.style.height = defaultBlockSize + "px";
+    gridBlock.style.width = defaultBlockSize + "px";
     gridBlock.style.margin = "0px";
     gridBlock.classList.add("block");
     gridContainer.appendChild(gridBlock);
-    console.log("grid block created");
   }
 }
 
 function addListeners() {
   let gridBlocks = document.querySelectorAll(".block");
-  console.log(gridBlocks);
   gridBlocks.forEach((block) => {
-    block.addEventListener('mouseover', () => {
-        block.style.backgroundColor = 'black';
+    block.addEventListener("mouseover", () => {
+      block.style.backgroundColor = "black";
     });
   });
 }
 
+function restartGrid() {
+  gridButton.addEventListener("click", () => {
+    let inputSize = parseInt(prompt("Insert grid size number. Maximum 100"));
+    if (Number.isInteger(inputSize) && (inputSize) <= 100) {
+      let currentGrid = document.querySelectorAll(".block");
+      currentGrid.forEach((block) => {
+        gridContainer.removeChild(block);
+      })
+      console.log(inputSize);
+      newGridSize = inputSize;
+      newSize = (512/newGridSize);
+      fillGridContainer();
+      addListeners();
+    } else if (inputSize > 100) {
+      alert('dont use number above 100!');
+    }
+  });
+}
+
+
+
 fillGridContainer();
 addListeners();
+restartGrid();
