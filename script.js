447b7f9cc2a16b6316b902d9e9fec51ef090f58b
onna3312 <ilyazor2@gmail.com>
2024-02-03 08:13:47 +0300

    Add erase functionality

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const gridContainer = document.querySelector(".grid-container");
 const gridButton = document.querySelector(".grid-button");
+const rainbowButton = document.querySelector(".rainbow-mode");
+const blackButton = document.querySelector(".normal-mode");
+const eraserButton = document.querySelector(".eraser-mode");
+
 let defaultBlockSize = 32;
 let newSize = null;
 let defaultGridSize = 16;
 let newGridSize = null;
+let currentMode = 'black';
 
+function generateRandomColor () {
+  let r = Math.floor(Math.random() * 255);
+  let g = Math.floor(Math.random() * 255);
+  let b = Math.floor(Math.random() * 255);
+  let result = "rgb(" + r + "," + g + "," + b + ")";
+  return result;
+}
 
 function fillGridContainer() {
   if (newSize !== null) {
@@ -29,9 +41,20 @@ function addListeners() {
   let gridBlocks = document.querySelectorAll(".block");
   gridBlocks.forEach((block) => {
     block.addEventListener("mouseover", () => {
-      block.style.backgroundColor = "black";
+    switch (currentMode) {  
+      case "black":
+        block.style.backgroundColor = "black";
+        break;
+      case "rainbow":
+        block.style.backgroundColor = generateRandomColor();
+        break;
+      case "eraser":
+        block.style.backgroundColor = "white";
+        break;
+    }
     });
   });
+
 }
 
 function restartGrid() {
@@ -53,8 +76,24 @@ function restartGrid() {
   });
 }
 
+function setModeButtons() {
+  blackButton.addEventListener("click", () => {
+    currentMode = "black";
+    console.log(currentMode);
+  })
 
+  rainbowButton.addEventListener("click", () => {
+    currentMode = "rainbow";
+    console.log(currentMode);
+  })
+
+  eraserButton.addEventListener("click", () => {
+    currentMode = "eraser";
+    console.log(currentMode);
+  })
+}
 
 fillGridContainer();
 addListeners();
 restartGrid();
+setModeButtons();
